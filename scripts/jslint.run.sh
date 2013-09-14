df254b683c8af066d1525d454642a0ef4fae405c
dr_bonzo <dr_bonzo@northslope.pl>
2013-09-14 03:41:51 +0200

    Adding JSLint script

---
+++ scripts/jslint.run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd public/js
+watch -n 0.5 jslint Activity.js RegexpFindAllActivity.js RegexpMatchInLineActivity.js RegexpMatchLineActivity.js RegexpReplaceActivity.js app.js monkey_patches.js
