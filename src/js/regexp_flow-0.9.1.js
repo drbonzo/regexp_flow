4eee4c9b1fae743362e25109c47fdfaa927e0415
dr_bonzo <dr_bonzo@northslope.pl>
2013-10-19 13:44:57 +0200

    RegexpFlow 0.9.1

--- src/js/regexp_flow-0.9.1.js
+++ src/js/regexp_flow-0.9.1.js
@@ -58,7 +58,7 @@ regexpFlow.config(['$routeProvider', function ($routeProvider) {
  */
 regexpFlow.controller('MainController', ['$scope', '$timeout', '$http', '$routeParams', '$location', function ($scope, $timeout, $http, $routeParams, $location) {
 
-    $scope.version = {name: 'RegexpFlow', version: '0.9.0'};
+    $scope.version = {name: 'RegexpFlow', version: '0.9.1'};
 
     $scope.infoPanelVisible = false;
 
