c30123a02f8eba8919c4e9ea36a3edb39b8cb9c3
dr_bonzo <dr_bonzo@northslope.pl>
2014-11-08 15:14:33 +0100

    Release 0.13.2

--- src/js/regexp_flow-0.13.2.js
+++ src/js/regexp_flow-0.13.2.js
@@ -60,7 +60,7 @@ regexpFlow.config(['$routeProvider', function ($routeProvider) {
  */
 regexpFlow.controller('MainController', ['$scope', '$timeout', '$http', '$routeParams', '$location', function ($scope, $timeout, $http, $routeParams, $location) {
 
-    $scope.version = {name: 'RegexpFlow', version: '0.13.1'};
+    $scope.version = {name: 'RegexpFlow', version: '0.13.2'};
 
     $scope.infoPanelVisible = false;
 
